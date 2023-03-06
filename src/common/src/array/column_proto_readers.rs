ae432d2de97d9a7c31889e4b9d5548f37d510daa
Shanicky Chen <peng@singularity-data.com>
2023-03-06 17:29:04 +0800

    feat(common): introduce SerialArrayBuilder and SerialArray (#8357)

--- src/common/src/array/column_proto_readers.rs
+++ src/common/src/array/column_proto_readers.rs
@@ -19,11 +19,12 @@ use byteorder::{BigEndian, ReadBytesExt};
 use paste::paste;
 use risingwave_pb::data::Array as ProstArray;
 
-use crate::array::value_reader::{PrimitiveValueReader, VarSizedValueReader};
+use crate::array::serial_array::Serial;
+use crate::array::value_reader::{I64ValueReader, PrimitiveValueReader, VarSizedValueReader};
 use crate::array::{
     Array, ArrayBuilder, ArrayImpl, ArrayMeta, ArrayResult, BoolArray, IntervalArrayBuilder,
     NaiveDateArrayBuilder, NaiveDateTimeArrayBuilder, NaiveTimeArrayBuilder, PrimitiveArrayBuilder,
-    PrimitiveArrayItemType,
+    PrimitiveArrayItemType, SerialArrayBuilder,
 };
 use crate::buffer::Bitmap;
 use crate::types::interval::IntervalUnit;
@@ -97,6 +98,13 @@ fn read_naive_date_time(cursor: &mut Cursor<&[u8]>) -> ArrayResult<NaiveDateTime
         .map_err(Into::into)
 }
 
+fn read_serial(cursor: &mut Cursor<&[u8]>) -> ArrayResult<Serial> {
+    I64ValueReader::read(cursor)
+        .map_err(|e| anyhow!("Failed to read i64 from Serial buffer: {}", e))
+        .map(Serial::from)
+        .map_err(Into::into)
+}
+
 pub fn read_interval_unit(cursor: &mut Cursor<&[u8]>) -> ArrayResult<IntervalUnit> {
     let mut read = || {
         let months = cursor.read_i32::<BigEndian>()?;
@@ -148,7 +156,8 @@ read_one_value_array! {
     { IntervalUnit, IntervalArrayBuilder },
     { NaiveDate, NaiveDateArrayBuilder },
     { NaiveTime, NaiveTimeArrayBuilder },
-    { NaiveDateTime, NaiveDateTimeArrayBuilder }
+    { NaiveDateTime, NaiveDateTimeArrayBuilder },
+    { Serial, SerialArrayBuilder }
 }
 
 fn read_offset(offset_cursor: &mut Cursor<&[u8]>) -> ArrayResult<i64> {
